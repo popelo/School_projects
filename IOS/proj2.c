b92ae5e7c75719f3064d7f093b61d6ee46c639c7
popelo <bednar.filip@yahoo.com>
2016-04-26 16:42:29 +0200

    base

--- IOS/proj2.c
+++ IOS/proj2.c
@@ -2,27 +2,59 @@
 #include <stdlib.h>
 #include <string.h>
 
-  int main( int argc, char *argv[] )
+//ošetriť pretečenie
+
+   int isnum( char *str )
 	{
-		//TRASH
-			(void)argc;
-		//TRASH
-		
-		long int P;
-		long int C;
-		int PT;
-		int RT;
+		int i = 0;
+		while ( str[i] != '\0')	
+				i++;
+
+		for ( int k = 0; k<i; k++ ) 
+			{
+				if ( str[k] < 48 || str[k] > 57 )
+					return 1;
+			}
+		return 0;
+	}
+  
+
+	
+  
+  int main( int argc, char *argv[] )
+	{		
+		//premenné vstup,práca sním
+		unsigned long P,C,PT,RT;
 		char *ptr; //pre strtol
-		
-		//potom podmienky
+		 
+		 
+
+		//ošetrenie vstupu 
+		if ( argc != 5 )
+			{
+				fprintf( stderr, "ERROR: Invalid input\n");
+				exit(1);
+			}
+		for (unsigned int i = 1 ; i <= 4 ; i++ )
+			{	
+				if ( ( isnum( argv[i] ) ) == 1 )
+					{
+						fprintf( stderr, "ERROR: Invalid input\n");
+						exit(1);
+					}
+			}
 
 		//predávanie argumentov
-		P = strtol( argv[1], &ptr, 10);
-		C = strtol( argv[2], &ptr, 10);
-		PT = atoi(argv[3]);
-		RT = atoi(argv[4]);
+		P = strtoul( argv[1], &ptr, 10);
+		C = strtoul( argv[2], &ptr, 10);
+		PT = strtoul( argv[3], &ptr, 10);
+		RT = strtoul( argv[4], &ptr, 10);
 		
-	
+		//TRASH
+		(void)P;
+		(void)C;
+		(void)PT;
+		(void)RT;	
 			
 	
 		return 0;
