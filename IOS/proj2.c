975135651fd867ce6f185e4fa3936bf801f851ce
popelo <bednar.filip@yahoo.com>
2016-05-02 13:55:13 +0200

    FINAL

--- IOS/proj2.c
+++ IOS/proj2.c
@@ -1,3 +1,7 @@
+//Filip Bednár
+//xbedna63
+//IOS proj2
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -55,7 +59,7 @@
   int main( int argc, char *argv[] )
 	{
 
-//PREMENNÉ/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////		
+//PREMENNÉ
 		int P; //počet procesov reprezentujúcich pasažierov 
 		int C; //kapacita vozíku
 		int PT; //maximálna doba(milisekundy) pre ktoré je generovaný nový proces pasažiera 
@@ -69,6 +73,7 @@
 		int main_p; 
 		int car_p;
 		int pas_p;
+		int id_p;
 
 		//premenné (semafory)
 		sem_t *sem_print;
@@ -192,7 +197,7 @@
 
 	if ( main_p == 0 )	//Subprocess pre vytáranie pasažierov
 		{
-			for (int k = 0; k < P; k++)
+			for (int i = 0; i < P; i++)
 				{
 					if (PT != 0) 
 						usleep(rand() % PT); //sleep pre pasažiera
@@ -209,71 +214,71 @@
 							exit(2);
 						}
 			
-			if ( pas_p == 0 )
-				{
-					*I = *I + 1;
-					int id_p = *I; //id pasažiera
-
-					sem_wait(sem_print); //štart a čakanie na vozík
-					fprintf(file, "%d %s %d %s\n", *A, ": P ", id_p, ": started");
-					*A += 1;
-					sem_post(sem_print);
-					sem_wait(sem_board);		
-
-					sem_wait(sem_print); //nastupovanie
-					fprintf(file, "%d %s %d %s\n", *A, ": P ", id_p, ": board");
-					*A += 1;
-
-					if (*N == C) //výpis nastúpenia
+					if ( pas_p == 0 )
 						{
-							fprintf(file, "%d %s %d %s\n", *A, ": P ", id_p, ": board last");
-							*N = 1;
+							*I = *I + 1;
+							id_p = *I; //id pasažiera
+							
+							sem_wait(sem_print); //štart a čakanie na vozík
+							fprintf(file, "%d %s %d %s\n", *A, ": P ", id_p, ": started");
 							*A += 1;
 							sem_post(sem_print);
-							sem_post(sem_last_board); //vozík je pripravený vyraziť
-						} 
-					else
-						{
-							fprintf(file, "%d %s %d %s %d\n", *A, ": P ", id_p, ": board order ", *N);
-							*N += 1;
+							sem_wait(sem_board);		
+
+							sem_wait(sem_print); //nastupovanie
+							fprintf(file, "%d %s %d %s\n", *A, ": P ", id_p, ": board");
 							*A += 1;
+
+							if (*N == C) //výpis nastúpenia
+								{
+									fprintf(file, "%d %s %d %s\n", *A, ": P ", id_p, ": board last");
+									*N = 1;
+									*A += 1;
+									sem_post(sem_print);
+									sem_post(sem_last_board); //vozík je pripravený vyraziť
+								} 
+							else
+								{
+									fprintf(file, "%d %s %d %s %d\n", *A, ": P ", id_p, ": board order ", *N);
+									*N += 1;
+									*A += 1;
+									sem_post(sem_print);
+								}
+
+							sem_wait(sem_unboard); //vystupovanie z vozíka
+							sem_wait(sem_print);
+							fprintf(file, "%d %s %d %s\n", *A, ": P ", id_p, ": unboard");
+							*A += 1;
+							if (*N == C)
+								{
+									fprintf(file, "%d %s %d %s\n", *A, ": P ", id_p, ": unboard last");
+									*A += 1;
+									*N = 1;
+									sem_post(sem_print);
+									sem_post(sem_last_unboard); // vystúpenie pasažierov z vozíka
+								}
+				 			else
+								{
+									fprintf(file, "%d %s %d %s %d\n", *A, ": P ", id_p, ": unboard order ", *N);
+									*N += 1;
+									*A += 1;
+									sem_post(sem_print);
+								}
+
+							sem_wait(sem_all_finished); //všetcia čakajú, ukončenie
+							sem_wait(sem_print);
+							fprintf(file, "%d %s %d %s\n", *A, ": P ", id_p, ": finished");
+							*A += 1;
+				
 							sem_post(sem_print);
+							sem_post(sem_main_finished); //posledný proces
+
+							exit(0);
+						}
 				}
 
-				
-				sem_wait(sem_unboard); //vystupovanie z vozíka
-				sem_wait(sem_print);
-				fprintf(file, "%d %s %d %s\n", *A, ": P ", id_p, ": unboard");
-				*A += 1;
-				if (*N == C)
-					{
-						fprintf(file, "%d %s %d %s\n", *A, ": P ", id_p, ": unboard last");
-						*A += 1;
-						*N = 1;
-						sem_post(sem_print);
-						sem_post(sem_last_unboard); // vystúpenie pasažierov z vozíka
-					}
-				 else
-					{
-						fprintf(file, "%d %s %d %s %d\n", *A, ": P ", id_p, ": unboard order ", *N);
-						*N += 1;
-						*A += 1;
-						sem_post(sem_print);
-					}
-
-				sem_wait(sem_all_finished); //všetcia čakajú, ukončenie
-				sem_wait(sem_print);
-				fprintf(file, "%d %s %d %s\n", *A, ": P ", id_p, ": finished");
-				*A += 1;
-
-				sem_post(sem_print);
-				sem_post(sem_main_finished); //posledný proces
-
-				exit(0);
-			}
-		}
-
 		exit(0);
+
 	}
 
 	if ( (car_p = fork()) < 0 ) //vytvorenie vozíka
@@ -295,13 +300,13 @@
 			*A += 1;
 			sem_post(sem_print);
 	
-			for (int k = 0; k < P/C; k++) //vozenie pasažierov
+			for (int j = 0; j < P/C; j++) //vozenie pasažierov
 				{
 					sem_wait(sem_print);
 					fprintf(file, "%d %s\n", *A, ": C  1 : load"); //nastupovanie
 					*A += 1;
 					sem_post(sem_print);
-					for (int j = 0; j < C; j++)
+					for (int k = 0; k < C; k++)
 						sem_post(sem_board);
 					
 					sem_wait(sem_last_board); //čakanie pre nastúpenie všetkých pasažierov
@@ -317,13 +322,12 @@
 					fprintf(file, "%d %s\n", *A, ": C  1 : unload");
 					*A += 1;
 					sem_post(sem_print);
-					for (int j = 0; j < C; j++)
+					for (int l = 0; l < C; l++) 
 						sem_post(sem_unboard);
-	
 					sem_wait(sem_last_unboard); //čakanie na všetkých pasažierov
 				}
 	
-			for (int j = 0; j < P; j++) //všetci dojazdili
+			for (int m = 0; m < P; m++) //všetci dojazdili
 					sem_post(sem_all_finished);
 			
 			sem_wait(sem_print);
@@ -335,16 +339,15 @@
 			exit(0);
 		}
 
-		for (int k = 0; k < P + 1; k++) //ukončenie hlavného procesu
-			sem_wait(sem_main_finished);
+	for (int n = 0; n < P + 1; n++) //ukončenie hlavného procesu
+		sem_wait(sem_main_finished);
 
-		unmap_var; //unmapovanie pamäte
-		sem_rm; //odstránenie semaforov
-		unmap_sem; //unmapovanie semaforov
-		if (fclose(file) == EOF) //zatvorenie súboru   
-            		fprintf(stderr, "%s\n", "ERROR: while closing file"); 
+	unmap_var; //unmapovanie pamäte
+	sem_rm; //odstránenie semaforov
+	unmap_sem; //unmapovanie semaforov
+	if (fclose(file) == EOF) //zatvorenie súboru   
+         	fprintf(stderr, "%s\n", "ERROR: while closing file"); 
                 
-		fprintf(stderr, "%s\n", "ERROR: forking main process");
-		exit(0);
+	exit(0);
 }
 	
